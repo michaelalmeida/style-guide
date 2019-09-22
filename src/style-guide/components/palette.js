02676b6c34cead0e702cc87e83ababa9327b353e
Michael Almeida <michaelralmeida@gmail.com>
2019-09-22 19:28:33 -0300

    Removal of unnecessary dependencies

---
+++ src/style-guide/components/palette.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Palette extends Component {
+    render() {
+        return (
+            <div>
+                Palette
+            </div>
+        )
+    }
+}
+
+export default Palette;
