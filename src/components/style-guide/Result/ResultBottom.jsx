59a9dfb01ecf16bc1a7e733621e9aefe584fe4a1
Michael Almeida <michaelralmeida@gmail.com>
2020-01-19 12:27:48 -0300

    Result Bottom Bar - Save button

---
+++ src/components/style-guide/Result/ResultBottom.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import BottomBar from './ResultStyles';
+
+import { Divider, Button } from 'antd';
+
+const ResultBottom = () => {
+  return (
+    <>
+      <Divider />
+      <Button>Save</Button>
+    </>
+  );
+};
+
+export default ResultBottom;
