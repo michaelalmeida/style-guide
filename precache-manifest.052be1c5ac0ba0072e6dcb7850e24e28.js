7d16e3e7048c457d306633de89bbc77894ee8cb4
Michael Almeida <michaelralmeida@gmail.com>
2020-08-19 19:51:41 -0300

    For GHpages

---
+++ precache-manifest.052be1c5ac0ba0072e6dcb7850e24e28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd6a7e8926ab0c074163146f6bd80944",
+    "url": "/index.html"
+  },
+  {
+    "revision": "994785e91a4859bb4aec",
+    "url": "/static/css/2.2e872fcd.chunk.css"
+  },
+  {
+    "revision": "0009d6e30d3270259476",
+    "url": "/static/css/main.b3ac6fdf.chunk.css"
+  },
+  {
+    "revision": "994785e91a4859bb4aec",
+    "url": "/static/js/2.8dc7e958.chunk.js"
+  },
+  {
+    "revision": "0009d6e30d3270259476",
+    "url": "/static/js/main.5fa4dec7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
